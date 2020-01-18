4b649ca058ffeb91b50a571b17b6616bbcc6096e
pi <jevharr@gmail.com>
2020-01-18 14:50:39 -0800

    inputs working, countdown working

--- I2C_Darkroom_Timer_v2.py
+++ I2C_Darkroom_Timer_v2.py
@@ -29,18 +29,6 @@ keypad = factory.create_keypad(keypad=KEYPAD, row_pins=ROW_PINS, col_pins=COL_PI
 # wipe LCD screen before we start
 mylcd.lcd_clear()
 
-# before we start the main loop
-time.sleep(1)
-mylcd.lcd_clear()
-
-# start fresh with 0:00:00
-def start_fresh():
-	getting_input = True
-	while getting_input == True:
-		keys = keypad.pressed_keys
-		getting_input = False
-	return str(keys[0])
-
 def printKey(key):
 	print(key)
 
@@ -49,9 +37,11 @@ def time_input(key):
 	time.sleep(0.2)
 	mylcd.lcd_clear()
 	global lcd_message_input1
-	lcd_message_input1 = 'ENTER TIME'
 	global lcd_message_input2
-	lcd_message_input2 = 'X:XX:XX'
+	global seconds
+	if key == 'A':
+		lcd_message_input1 = 'ENTER TIME'
+		lcd_message_input2 = 'X:XX:XX'
 	mylcd.lcd_display_string(lcd_message_input1, 1)
 	mylcd.lcd_display_string(lcd_message_input2, 2)
 	m = re.compile("X")
@@ -60,58 +50,71 @@ def time_input(key):
 #	p = re.compile("[X:]+$")
 	q = re.compile("\d+")
 	r = re.compile("\d+$")
-
-	while re.search(".$", lcd_message_input2).group() == 'X':
+	print("time_input.key: ", key, " isnumeric: ", key.isnumeric())
+	if key.isnumeric() and re.search(".$", lcd_message_input2).group() == 'X':
 	# get_keypress
-		for i in range(len(o.search(lcd_message_input2, 0).group())):
-#			keys = keypad.pressed_keys
-#			if keys:
-			lcd_message_input2 = m.sub(key, lcd_message_input2, 1)
-			mylcd.lcd_display_string(lcd_message_input2, 2)
-			time.sleep(0.1)
-	seconds = int(q.search(lcd_message_input2, 0).group()) * 60 + int(r.search(lcd_message_input2, 0).group())
-	while True:
-#		keys = keypad.pressed_keys
-#		if keys:
-		result = a_switch(str(keys[0]), seconds)
+		print("is numeric")
+#		for i in range(len(o.search(lcd_message_input2, 0).group())):
+		print("time_input -> keypress: ", key)
+		lcd_message_input2 = m.sub(key, lcd_message_input2, 1)
+		print(lcd_message_input2)
+		print(q.search(lcd_message_input2, 0).group())
+		mylcd.lcd_display_string(lcd_message_input2, 2)
+	if key.isnumeric() and re.search(".$", lcd_message_input2).group() != 'X':
+		seconds = int(q.search(lcd_message_input2, 0).group()) * 60 + int(r.search(lcd_message_input2, 0).group())
+		print(seconds, " seconds")
 	return(0)
 
-def previous_time(seconds):
+def key_press(key):
+	return(key)
+
+def previous_time(x):
 	mylcd.lcd_display_string(lcd_message_input1, 1)
 	mylcd.lcd_display_string(lcd_message_input2, 2)
-#	seconds = int(q.search(lcd_message_input2, 0).group()) * 60 + int(r.search(lcd_message_input2, 0).group())
-	timing = 1
-	while timing:
-		keys = keypad.pressed_keys
-		if str(keys[0]) == 'D':
-			timing = countdown_timer(seconds)
+	time_input(x)
 	return(0)
 
 # count down to 0 from the entered value
 def countdown_timer(x, now=datetime.datetime.now):
 	target = now()
 	one_second_later = datetime.timedelta(seconds=1)
-	for remaining in range(x, -1, -1):
+	for remaining in range(seconds, -1, -1):
 		target += one_second_later
 		print(datetime.timedelta(seconds=remaining), 'remaining', end='\r')
 		mylcd.lcd_display_string('Time remaining: ', 1)
 		mylcd.lcd_display_string(str(datetime.timedelta(seconds=remaining)), 2)
 		time.sleep((target - now()).total_seconds())
+	mylcd.lcd_clear()
+	mylcd.lcd_display_string('A to start', 1)
+	mylcd.lcd_display_string('B to repeat', 2)
+
 	return 0
 #	print('\nTIMER ended')
 
 def top_switch(key):
+	print("top_switch: ", key)
 	mylcd.lcd_clear()
-	mylcd.lcd_display_string('A to start', 1)
-	mylcd.lcd_display_string('B to repeat', 2)
-	seconds = 0
 	the_switch = {
 		'A': time_input,
-		'B': previous_time
+		0 : time_input,
+		1: time_input,
+		2: time_input,
+		3: time_input,
+		4: time_input,
+		5: time_input,
+		6: time_input,
+		7: time_input,
+		8: time_input,
+		9: time_input,
+		'B': previous_time,
+		'D': countdown_timer
 	}
 	# get the function from the_switch
-	func = the_switch.get(key, lambda: "Invalid")
-	return(func(seconds))
+	global mode
+	mode = the_switch.get(key)
+	func = the_switch.get(key) #, lambda: "Invalid")
+	func(str(key))
+	return(0)
 
 def a_switch(key, seconds):
 	the_switch = {
@@ -120,13 +123,20 @@ def a_switch(key, seconds):
 		'D': countdown_timer
 	}
 	func = the_switch.get(key, lambda: "Invalid")
-	return(func(seconds))
+	return(func(key))
 
 #if __name__ == '__main__':
 	#while True:
 
 
 #print("testing pad4pi")
+mylcd.lcd_display_string('A to start', 1)
+mylcd.lcd_display_string('B to repeat', 2)
+
+#global lcd_message_input1
+lcd_message_input1 = 'ENTER TIME'
+#global lcd_message_input2
+lcd_message_input2 = 'X:XX:XX'
 keypad.registerKeyPressHandler(top_switch)
 
 try:
